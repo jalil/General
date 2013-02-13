1ce8dad6dd260f93f724fb6e8c67a4efbd22f701
Braden <administrator@M4300.(none)>
2013-02-13 07:36:44 -0600

    Fixed documentation for Assembly language solutions

--- Solutions Archive/Assemblyx86-Exit_Syscall.s
+++ Solutions Archive/Assemblyx86-Exit_Syscall.s
@@ -15,8 +15,8 @@
 
 .globl _start
 _start:
-movl $1, %eax #this is the linux kernel command number (syscall) for exiting a program
-movl $0, %ebx #this is the status number we will return the OS. Change these around later to see what happens
-int $0x80 #this wakes up the kernel to run the exit command
+movl $1, %eax 	#this is the linux kernel command number (syscall) for exiting a program
+movl $0, %ebx 	#this is the status number we will return the OS.
+int $0x80 	#this is the interrupt number for running syscalls
 
 
--- Solutions Archive/Assemblyx86-Min_Value.s
+++ Solutions Archive/Assemblyx86-Min_Value.s
@@ -14,28 +14,29 @@
 .section .data
 
 data_items:
- .long 213,23,35,56,24,23,123,32,12,1,6,3,255
+ .long 213,23,35,56,24,23,123,32,12,1,6,3,255		#Data items
 
 .section .text
 
 .globl _start
  
 _start:
- movl $0, %edi
- movl data_items(,%edi,4), %eax
- movl %eax, %ebx
+ movl $0, %edi						#Move the a literal 0 into the %edi register
+ movl data_items(,%edi,4), %eax				#Use the %edi register as an index to access data_items
+ movl %eax, %ebx					#Since this is the first execution, the first num will be min
 
-start_loop:
- cmpl $255, %eax
- je end_loop
- incl %edi
- movl data_items(,%edi,4), %eax
- cmpl %ebx, %eax
- jge start_loop
- movl %eax, %ebx
- jmp start_loop
+start_loop:						
+ cmpl $255, %eax					#If %eax == 255, then we quit execution
+ je end_loop		
+ incl %edi						#Increment index
+ movl data_items(,%edi,4), %eax				#Retrieve the next element
+ cmpl %ebx, %eax					#Compare the next element with current element
+ jge start_loop						#If the next element is greater, loop
+ movl %eax, %ebx					#Else, replace the max register wit next element
+ jmp start_loop						#Unconditional loop
+
 
 end_loop:
- movl $1, %eax
- int $0x80
+ movl $1, %eax						#Move syscall number into %eax
+ int $0x80						#Interrupt
  
