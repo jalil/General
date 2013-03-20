d6715323e0ccf7e68c512a6a4b760a78841b214a
Braden Lenz <blenz3@uic.edu>
2013-03-19 23:33:20 -0500

    Addition of Connect 4 game code

--- Projects Archive/Connect4/connect4.py
+++ Projects Archive/Connect4/connect4.py
@@ -3,7 +3,7 @@ import random
 class ConnectFour(object):
 	def __init__(self):
 		self.board = []
-        	for i in range(6):
+		for i in range(6):
 			self.board.append([])
 			for j in range(7):
 				self.board[i].append(None)
@@ -29,65 +29,104 @@ class ConnectFour(object):
 		else:
 			return None
 
-	def match_in_direction(self, row, column, step_row, step_col):
-		"""
-		Counts how many chips, starting from (row, column) and moving in the
-		direction (step_row, step_col), match the player who occupies (row,column).
-
-		Arguments:
-		row: an integer between 0 and 5
-		column: an integer between 0 and 6
-		step_row: an integer, either -1, 0, or 1
-		step_col: an integer, either -1, 0, or 1
-
-		This function first checks which player occupies (row,column).  If no
-		player occupies this position, 0 is returned.  Otherwise, the function
-		then continues checking (row + step_row, column + step_column), (row +
-		2*step_row, column + 2*step_column), and so forth, continuing until a
-		location is either empty, the board edge is reached, or a position is
-		occupied by an opposing player.  The function then returns how many
-		chips (including the first chip at (row, column), match.  Note if the
-		function returns 4 or more, this is a winning position. 
-		"""
-        	assert row >= 0 and row < 6 and column >= 0 and column < 7
-		assert step_row != 0 or step_col != 0 # (0,0) gives an infinite loop
-
-		intPlayer = 0
-
-        	if self.board[row][column] != None:
-			if self.board[row][column] == 'X':
-				intPlayer = 1
-			elif self.board[row][column] == 'O':
-				intPlayer = 2
-		else:
+	def match_in_direction(self,row,col,srow,scol, uchar):
+		if row + srow > 5 or col + scol > 6:
 			return None
-
-		counter = 1
-		loop = True
-		while loop == True:
-			if counter == 4:
-				return counter
-			if (row + (counter * step_row) <= 5 and row + (counter * step_row) >= 0) and (column + (counter * step_col) <= 6 and column + (counter * step_col) >= 0):
-				if self.get_position((row + (step_row * counter)), (column + (step_col * counter))) == intPlayer:
-					counter += 1
-				else:
-					counter = 1
+		
+		if self.board[row + srow][col + scol] != None:
+			if uchar == self.board[row + srow][col + scol]:
+				return 1
 			else:
-				loop = False
-		return counter
+				return 0
+		else:
+			return 'n'
+		
+	def checkDiag(self):
+		lstOuts = []
+		#Checking along the col = 0 rows
+		for row in range(3, 6):
+			repeatChar = 'X'
+			repeatCount = 0
+			rcounter = row
+			ccounter = 0
+			while ccounter <= row:
+				out = self.match_in_direction(rcounter,ccounter,-1,1,repeatChar)
+				if out == 1:
+					repeatCount += 1
+					lstOuts.append((rcounter,ccounter))
+				elif out == 0:
+					repeatChar = self.board[rcounter][ccounter]
+					repeatCount = 1
+				else:
+					break
+				if repeatCount == 4:
+					print lstOuts
+					return self.get_position(rcounter, ccounter)
+				rcounter -= 1
+				ccounter += 1
+		
+		#Checking along row = 5 cols
+		'''for col in range(1,4):
+			repeatChar = ''
+			repeatCount = 1
+			rcounter = 5
+			
+			while counter >= (counter - col): #i.e. col = 2 checks 5 grid coords
+				out = self.match_in_direction(counter, col, -1, 1, self.board[counter][col])  
+				if out == 1:
+					repeatCount += 1
+					lstOuts.append((counter,col))
+				elif out == 0:
+					repeatChar = self.board[counter][col]
+					repeatCount = 1
+				else:
+					break
+				if repeatCount == 4:
+					print lstOuts
+					return self.get_position(counter, col)
 
-	def genDiag(self, lBound, uBound, pORm, vORh, colStart = 0):
+			counter -= 1'''
+		return None
+
+	def genDiag(self, lBound, uBound, intOp, colStart = 0):
 	#Generalized diagonal test -- child of diagTest
 	#lBound == lower bound of rows
 	#uBound == upper bound of rows
 	#pORm == char 'p' <- (plus == asc diags), char'm' <- (minus == desc diags)
 	#vORh == char 'v' or 'h': if 'v' then iterate over rows, elif 'h' iterate over columns
-
-		if vORh == 'v':
+		if intOp == 1:
 			for i in range(lBound, uBound):
+				repeatChar = 0
+				repeatChar = 0
+				x = 6
+				y = 0
+				while x >= 0:
+					if self.board[i - y][x] != None:
+						if repeatChar == '':
+							repeatChar = self.board[i - y][x]
+							repeatCount = 0
+						elif self.board[i - y][x] == repeatChar:
+							repeatCount += 1
+						elif self.board[i - y][x] != repeatChar:
+							repeatChar = self.board[i - y][x]
+							repeatCount = 1
+						if repeatCount == 4:
+							return self.get_position(i - y, x)
+					else:
+						repeatChar = ''
+						repeatCount = 0
+
+					x -= 1
+				return None
+
+
+
+
+		elif intOp == 2:
+			for i in range(3, 6):
 				repeatCount = 0
 				repeatChar = ''
-				x = colStart # <- depth of diag
+				x = 0 # <- depth of diag
 				while x < i:
 					if self.board[i - x][x] != None:
 						if repeatChar == '':
@@ -106,40 +145,75 @@ class ConnectFour(object):
 						else:
 							repeatChar = ''
 							repeatCount = 0
-					if pORm == 'p':
-						x += 1
-					else:
-						x -= 1
+					x += 1
 			return None
-		elif vORh == 'h':
-			for j in range(lBound, uBound):
+		elif intOp == 3:
+			for j in range(1, 4):
 				repeatCount = 0
 				repeatChar = ''
-				y  = colStart
-				while y < 6:
-					if self.board[y][j] != None:
+				depth = 0
+				rowStart = 6
+				
+				
+				#self.board[rowStart - depth][colStart + depth]
+				
+				
+				
+				
+				
+				
+				k = j
+				y  = 0
+				while y < 6 and k < 7:
+					if self.board[y][k] != None:
 						if repeatChar == '':		
-							repeatChar = self.board[y][j]
+							repeatChar = self.board[y][k]
 							repeatCount = 1
-						elif self.board[y][j] == repeatChar:
+						elif self.board[y][k] == repeatChar:
 							repeatCount +=  1
-						elif self.board[y][j] != repeatChar:
-							repeatChar = self.board[y][j]
+						elif self.board[y][k] != repeatChar:
+							repeatChar = self.board[y][k]
 							repeatCount = 1
 		
 						if repeatCount == 4:
-							return self.get_position(y, j)
+							return self.get_position(y, k)
 					else:
-						if float(y) / j > 0.6 and j != 0:
+						if float(y) / k > 0.6 and k != 0:
 							break
 						else:
 							repeatChar = ''
 							repeatCount = 0
-					if pORm == 'p':
-						y += 1
-					else:
-						y -= 1
+
+					y += 1
+					k += 1
+
 			return None
+
+
+		elif intOp == 4:
+			for j in range(4, 7):
+				repeatCount = 0
+				repeatChar = ''
+				y = 6
+				y = 0
+				while y >= 0:
+					if self.board[i - y][x] != None:
+						if repeatChar == '':
+							repeatChar = self.board[i - y][x]
+							repeatCount = 0
+						elif self.board[i - y][x] == repeatChar:
+							repeatCount += 1
+						elif self.board[i - y][x] != repeatChar:
+							repeatChar = self.board[i - y][x]
+							repeatCount = 1
+						if repeatCount == 4:
+							return self.get_position(i - y, x)
+					else:
+						repeatChar = ''
+						repeatCount = 0
+
+					x -= 1
+				return None
 
 
 	def diagTest(self):
@@ -152,19 +226,19 @@ class ConnectFour(object):
 	#	
 	
 		#Asc-Asc Diagonals paired tests:
-		pOut1 = self.genDiag(3,6,'p','v', 0) # <- optimized start location for (0,0) vertical portion of grid
-		pOut2 = self.genDiag(1,4, 'p', 'h', 0) # <- optimized start location for (6,7) horizontal portion of grid
-		print self.genDiag(3,6,'m','v',6)
+		print self.genDiag(3,6,2,0) # <- optimized start location for (0,0) vertical portion of grid
+		print self.genDiag(1,4, 3,0) # <- optimized start location for (6,7) horizontal portion of grid
+		print self.genDiag(3,6,1,6), 'minus'
 		#dOut2 = 
 
-		if pOut1 != None and pOut2 != None:
-			return pOut1
-		elif pOut1 != None:
-			return pOut1
-		elif pOut2 != None:
-			return pOut2
-		else:
-			return None
+#		if pOut1 != None and pOut2 != None:
+#			return pOut1
+#		elif pOut1 != None:
+#			return pOut1
+#		elif pOut2 != None:
+#			return pOut2
+#		else:
+#			return None
 
 		
 	def vertTest(self):
@@ -307,3 +381,29 @@ game.print_board()
 print game.vertTest()
 print game.horTest()
 print game.diagTest()
+print "\n\n\n\n\n\n\n\n\n\n"
+
+agame = ConnectFour()
+agame.play_turn(1,3)
+for i in range(2):
+	for j in range(7):
+		if j % 2 == 0:
+			agame.play_turn(1,j)
+		else:
+			agame.play_turn(2,j)
+			
+agame.play_turn(1,1)
+agame.play_turn(2,1)
+agame.play_turn(2,1)
+agame.play_turn(1,1)
+agame.play_turn(1,2)
+agame.play_turn(2,2)
+agame.play_turn(1,2)
+agame.play_turn(1,3)
+agame.play_turn(1,3)
+agame.play_turn(1,4)
+#agame.board(0,3) = 'X'
+agame.play_turn(2,0)
+agame.play_turn(1,0)
+agame.print_board()
+print agame.checkDiag()
