823025eaf0a74cd9fad94864cbbf1c0370679fe7
Braden Lenz <blenz3@uic.edu>
2013-04-08 10:24:56 -0500

    Addition of character frequency analysis in C++

---
+++ Solutions Archive/C++-CharFrequency.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+
+void printFreqs(map <char, int> &mFreqs, int len);
+
+int main( int argc, const char *argv[]) {
+	int i;
+	string line;
+	map <char, int> freqs;
+
+	cout << "Please give me a line of text: ";
+	getline(cin, line);
+
+	for (i = 0; i < line.length(); i++) {
+		if (freqs.find(line[i]) == freqs.end()) {
+			freqs[line[i]] = 1;
+		}
+		else {
+			freqs[line[i]] += 1;
+		}
+	}
+
+	map<char, int>::iterator it;
+	for (map<char, int>::iterator it=freqs.begin(); it!=freqs.end(); it++) {
+		cout << "'" <<  it->first << "'" << " => " << it->second << " freq: " << ((it->second / (double)line.length()) * 100) << "%" << endl;
+	}
+
+	cout << "Goodbye!" << endl;
+	return 0;
+}
+
