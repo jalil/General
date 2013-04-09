3ed837d95ecea2c59f7f7c2efda8fe64f9dd2c6a
Braden Lenz <blenz3@uic.edu>
2013-04-08 20:35:43 -0500

    Addition of word frequency program

---
+++ Solutions Archive/C++-WordFrequency.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+
+int main( int argc, const char *argv[]) {
+	bool cont = true;
+	int i;
+	string word, line;
+	map <string, int> freqs;
+
+	cout << "Please give me a line of text: ";
+	getline(cin, line);
+
+	do {
+		i = line.find(" ");
+		if (i != string::npos) {
+			cout << line << endl;
+			word = line.substr(0, i);
+			line = line.substr(i + 1, line.length() - 1);	
+		}
+		else {
+			cont = false;
+			word = line;
+		}
+		
+		if (freqs.find(word) == freqs.end()) {
+			freqs[word] = 1;
+		}
+		else {
+			freqs[word] += 1;
+		}
+	}
+	while (line.find(" ") != string::npos || cont == true);
+
+	map<string, int>::iterator it;
+	for ( it=freqs.begin(); it!=freqs.end(); it++) {
+		cout << "'" <<  it->first << "'" << " => " << it->second << " freq: " << ((it->second / (double)line.length()) * 100) << "%" << endl;
+	}
+
+	cout << "Goodbye!" << endl;
+	return 0;
+}
+
