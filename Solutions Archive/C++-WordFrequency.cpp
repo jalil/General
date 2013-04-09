25ced1f5a03a361a867b619301b172d465de9c5e
Braden Lenz <blenz3@uic.edu>
2013-04-08 20:39:54 -0500

    House cleaning on word frequency C++ program

--- Solutions Archive/C++-WordFrequency.cpp
+++ Solutions Archive/C++-WordFrequency.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main( int argc, const char *argv[]) {
 	bool cont = true;
-	int i;
+	int i, wordCount = 0;
 	string word, line;
 	map <string, int> freqs;
 
@@ -17,7 +17,6 @@ int main( int argc, const char *argv[]) {
 	do {
 		i = line.find(" ");
 		if (i != string::npos) {
-			cout << line << endl;
 			word = line.substr(0, i);
 			line = line.substr(i + 1, line.length() - 1);	
 		}
@@ -32,12 +31,13 @@ int main( int argc, const char *argv[]) {
 		else {
 			freqs[word] += 1;
 		}
+		wordCount += 1;
 	}
 	while (line.find(" ") != string::npos || cont == true);
 
 	map<string, int>::iterator it;
 	for ( it=freqs.begin(); it!=freqs.end(); it++) {
-		cout << "'" <<  it->first << "'" << " => " << it->second << " freq: " << ((it->second / (double)line.length()) * 100) << "%" << endl;
+		cout << "'" <<  it->first << "'" << " => " << it->second << " freq: " << ((it->second / (double)wordCount) * 100) << "%" << endl;
 	}
 
 	cout << "Goodbye!" << endl;
