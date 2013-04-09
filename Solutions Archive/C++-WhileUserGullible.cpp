8d44493c0479a879b605b4911b694f0040b70261
Braden Lenz <blenz3@uic.edu>
2013-04-09 15:36:39 -0500

    Fixed bug in while user gullible program solution

--- Solutions Archive/C++-WhileUserGullible.cpp
+++ Solutions Archive/C++-WhileUserGullible.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main(int argc, const char *argv[]) {
 	int i, o;
-	i = 1;
 	o = 0;
 	do {
 		o += 1;
 		cout << "Enter any number aside from " << o << ": ";
 		cin >> i;
 	} while( i != o);
+	cout << "You weren't supposed to type in " << i << "!" << endl;
 	return 0;
 }
 
