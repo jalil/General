42f1700d7b774bfaf7a7b7520f9bc62722c7ac4e
Braden Lenz <blenz3@uic.edu>
2013-04-14 00:16:42 -0500

    Added unique character checking algorithm

---
+++ Solutions Archive/C++-UniqueChar.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+map<char, int> makeMap(string s) {
+	int i;
+	map<char, int> mString;
+
+	for (i = 0; i < s.length(); i++) {
+		if (mString.find(s[i]) == mString.end()) {
+			mString[s[i]] = 1;
+		}
+		else {
+			mString[s[i]] += 1;
+		}
+	}
+	return mString;
+}
+
+bool isUnique(map<char, int> sMap) {
+	for (map<char, int>::iterator it = sMap.begin(); it != sMap.end(); it++) {
+		if (it->second >= 2) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main(int argc, const char *argv[]) {
+	string s;
+
+	cout << "Give me a string to check: ";
+	cin >> s;
+	
+	cout << "Are all of the characters in the string unique?\n  --> " << (isUnique(makeMap(s)) == 1 ? "True" : "False") << endl;
+	return 0;
+}
+
