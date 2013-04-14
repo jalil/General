a85d8b09653ccf1a14812f4b87f27520f53c4564
Braden Lenz <blenz3@uic.edu>
2013-04-14 00:18:05 -0500

    Updated user interaction features in unique character program

--- Solutions Archive/C++-UniqueChar.cpp
+++ Solutions Archive/C++-UniqueChar.cpp
@@ -34,7 +34,7 @@ int main(int argc, const char *argv[]) {
 	cout << "Give me a string to check: ";
 	cin >> s;
 	
-	cout << "Are all of the characters in the string unique?\n  --> " << (isUnique(makeMap(s)) == 1 ? "True" : "False") << endl;
+	cout << "Are all of the characters in the string, '" << s << "', unique?\n  --> " << (isUnique(makeMap(s)) == 1 ? "True" : "False") << endl;
 	return 0;
 }
 
