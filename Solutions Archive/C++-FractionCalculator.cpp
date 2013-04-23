7bd34fc7dde8605ea546ce4cae7714b27f92be04
Braden <administrator@M4300.(none)>
2013-04-23 17:30:47 -0500

    Major revisions to c++ fraction calculator class

--- Solutions Archive/C++-FractionCalculator.cpp
+++ Solutions Archive/C++-FractionCalculator.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-
-using namespace std;
 
 class Fraction {
 	private:
@@ -18,23 +15,19 @@ class Fraction {
 		Fraction();
 		Fraction(int, int);
 		
-		//destructors
-		~Fraction();
-
-		//computation options
-		Fraction multiply(Fraction b);
-		Fraction divide(Fraction b);
-		Fraction add(Fraction b);
-		Fraction subtract(Fraction b);
+		//operator overloading
+		Fraction operator+ (Fraction);
+		Fraction operator- (Fraction);
+		Fraction operator* (Fraction);
+		Fraction operator/ (Fraction);
 		
 		//util methods
-		string print();
-		string intToStr(int n);
+		std::string print();
 		int GCD(int a, int b);
 		void reduce();
 };
 //Method declarations
-void Fraction::setNumerator(int n) { numerator = n;}
+void Fraction::setNumerator(int n) { numerator = n; }
 
 void Fraction::setDenominator(int d) { denominator = d;}	
 
@@ -53,44 +46,48 @@ Fraction::Fraction(int a, int b) {
 	reduce();
 }
 
-Fraction::~Fraction() {}
-
-Fraction Fraction::multiply(Fraction b) {
-	Fraction c((numerator * b.getNumerator()), (denominator * b.getDenominator()));
-	return c;
+Fraction Fraction::operator* (Fraction b) {
+	return Fraction ((numerator * b.getNumerator()), (denominator * b.getDenominator()));
 }
 
-Fraction Fraction::divide(Fraction b) {
-	Fraction c((numerator * b.getDenominator()), (denominator * b.getNumerator()));
-	return c;
+Fraction Fraction::operator/ (Fraction b) {
+	return Fraction ((numerator * b.getDenominator()), (denominator * b.getNumerator()));
 }
 
-Fraction Fraction::add(Fraction b) {
+Fraction Fraction::operator+ (Fraction b) {
 	int numc, denc;
 
-	numc = (numerator * b.getDenominator()) + (denominator * b.getNumerator());
-	denc = denominator * b.getDenominator();
-
-	return Fraction(numc, denc);
+	if (numerator == 0 || b.getNumerator() == 0) {
+		if (numerator != 0) {
+			return Fraction(numerator, denominator);
+		}
+		else {
+			return Fraction(b.getNumerator(), b.getDenominator());
+		}
+	}
+	else {
+		return Fraction((numerator * b.getDenominator()) + (denominator * b.getNumerator()), denominator * b.getDenominator());
+	}
 }
 
-Fraction Fraction::subtract(Fraction b) {
+Fraction Fraction::operator- (Fraction b) {
 	int numc, denc;
-	
-	numc = (numerator * b.getDenominator()) - (denominator * b.getNumerator());
-	denc = denominator * b.getDenominator();
 
-	return Fraction(numc, denc);
+	if (numerator == 0 || b.getNumerator() = 0) {	
+		if (numerator != 0) {
+			return Fraction(numerator, denominator);
+		}
+		else {
+			return Fraction(b.getNumerator(), b.getDenominator());
+		}
+	}
+	else {
+		Fraction((numerator * b.getDenominator()) - (denominator * b.getNumerator()), (denominator * b.getDenominator());
+	}
 }	
 
-string Fraction::print() {
-	return intToStr(numerator) + " / " + intToStr(denominator);
-}
-
-string Fraction::intToStr(int n) {
-	stringstream ss;
-	ss << n;
-	return ss.str();
+std::string Fraction::print() {
+	return std::to_string(numerator) + " / " + std::to_string(denominator);
 }
 
 int Fraction::GCD(int a, int b) {
@@ -118,43 +115,51 @@ int Fraction::GCD(int a, int b) {
 }
 
 void Fraction::reduce() {
-	int gcd = GCD(numerator, denominator);
-	setNumerator(numerator / gcd);
-	setDenominator(denominator / gcd);
+	int gcd;
+		
+	if (numerator == 0 || denominator == 0) {
+		numerator = 0;
+		denominator = 0;
+	}
+	else {
+		gcd = GCD(numerator, denominator);
+		setNumerator(numerator / gcd);
+		setDenominator(denominator / gcd);
+	}
 }
 
 
 int main(int argc, const char *argv[]) {
 	int a, b;
 
-	cout << "Give me a numerator: ";
-	cin >> a;
-	cout << "Give me a denominator: ";
-	cin >> b;
+	std::cout << "Give me a numerator: ";
+	std::cin >> a;
+	std::cout << "Give me a denominator: ";
+	std::cin >> b;
 
 	Fraction a1(a,b);
-	cout << "The first fraction is " << a1.print() << endl;
+	std::cout << "The first fraction is " << a1.print() << std::endl;
 
-	cout << "Give me a second numerator: ";
-	cin >> a;
-	cout << "Give me a second denominator: ";
-	cin >> b;
+	std::cout << "Give me a second numerator: ";
+	std::cin >> a;
+	std::cout << "Give me a second denominator: ";
+	std::cin >> b;
 
 	Fraction a2(a,b);
-	cout << "The second fraction is " << a2.print() << endl;
+	std::cout << "The second fraction is " << a2.print() << std::endl;
 
 	//Computation
-	cout << "The following operations are completed: " << endl;	
+	std::cout << "The following operations are completed: " << std::endl;	
 
-	Fraction a3 = a1.add(a2);
-	cout << "ADDITION: " << a1.print() << " plus " << a2.print() << " is " << a3.print() << endl;
-	a3 = a1.subtract(a2);
-	cout << "SUBTRACTION: " << a1.print() << " minus " << a2.print() << " is " << a3.print() << endl;
-	a3 = a1.multiply(a2);
-	cout << "MULTIPLICATION: " << a1.print() << " multiplied by " << a2.print() << " is " << a3.print() << endl;
+	Fraction a3 = a1 + a2;
+	std::cout << "ADDITION: " << a1.print() << " plus " << a2.print() << " is " << a3.print() << std::endl;
+	a3 = a1 - a2;
+	std::cout << "SUBTRACTION: " << a1.print() << " minus " << a2.print() << " is " << a3.print() << std::endl;
+	a3 = a1 * a2;
+	std::cout << "MULTIPLICATION: " << a1.print() << " multiplied by " << a2.print() << " is " << a3.print() << std::endl;
 
-	a3 = a1.divide(a2);
-	cout << "DIVISION: " << a1.print() << " divided by " << a2.print() << " is " << a3.print() << endl;
+	a3 = a1 / a2;
+	std::cout << "DIVISION: " << a1.print() << " divided by " << a2.print() << " is " << a3.print() << std::endl;
 
 	return 0;
 }
