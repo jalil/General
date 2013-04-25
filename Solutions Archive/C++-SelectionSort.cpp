1284ece44bb8c5eff8b73c57e1b48531b474c941
Braden <administrator@M4300.(none)>
2013-04-25 14:22:33 -0500

    Updated C++ Selection Sort algorithm

---
+++ Solutions Archive/C++-SelectionSort.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+template <typename T>
+void selectionSort(std::vector<T>& v) {
+	//sorts --> number of swaps, i --> integral offset from first pointer location
+	unsigned int sorts = 0, i = 0;
+	//it --> main iterator, mint --> minimum value location
+	typename std::vector<T>::iterator it, mint;
+	while (i != v.size() - 1) {
+		//initialize mint --> first element of unsorted sub-vector
+		mint = v.begin() + i;
+		for (typename std::vector<T>::iterator it = i + v.begin(); it != v.end(); ++it) {
+			if (it != v.end()) {			
+				if (*mint > *it) {
+					mint = it;
+					sorts++;
+				}
+			}
+		}
+		if (sorts != 0) {
+			std::iter_swap(v.begin() + i, mint);
+		}
+		i = i + 1;
+	}
+}
+
+template <typename T>
+std::string printVector(std::vector<T> v) {
+	std::string s = "<";
+	for (typename std::vector<T>::iterator it = v.begin(); it != v.end(); it++) {
+		s += std::to_string(*it) + ", ";
+	}
+	return s.substr(0, s.length() - 2) + ">";
+} 
+
+int main(int argc, const char *argv[]) {
+	int i,j,k;
+	std::vector<int> vIn;
+
+	std::cout << "How many numbers in your vector?\n  -->  ";
+	std::cin >> j;
+
+	for (i = 0; i < j; i++) {
+		std::cout << "Element " << i + 1 << ": ";
+		std::cin >> k;
+		vIn.push_back(k);
+	}
+	
+	std::cout << "The unordered vector is: " << printVector(vIn) << std::endl;
+
+	selectionSort(vIn);
+
+	std::cout << "The ordered vector is: " << printVector(vIn) << std::endl;	
+
+	return 0;
+}
+ 
