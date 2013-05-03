47f9a83ca8330ed20ff249cee4f0f0a0ee233a30
Braden <administrator@M4300.(none)>
2013-05-03 14:32:51 -0500

    Ruby project euler solution 1

---
+++ Problem-Sets/Project-Euler/P1-MultiplesOf3And5/Ruby-P1.rb
@@ -0,0 +1,17 @@
+def mults3or5(u)
+	s = 0
+	for i in (0..u - 1)
+		if i % 5 == 0 or i % 3 == 0
+			s += i
+		end
+	end
+	s
+end
+
+def main()
+	print "What  is the upper bound of the summation of multiples of 3 or 5?\n  -->  "
+	i = gets.chomp().to_i
+	puts "#{mults3or5(i)}"
+end	
+
+main()
