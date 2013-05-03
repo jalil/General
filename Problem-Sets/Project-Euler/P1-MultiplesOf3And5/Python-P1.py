2873583a64b0fd6557473ded60872f026859f4a7
Braden <administrator@M4300.(none)>
2013-05-03 14:26:35 -0500

    Added main function

--- Problem-Sets/Project-Euler/P1-MultiplesOf3And5/Python-P1.py
+++ Problem-Sets/Project-Euler/P1-MultiplesOf3And5/Python-P1.py
@@ -2,5 +2,8 @@ def mults3or5(u):
 	s = 0
 	return sum([s + i for i in range(u) if i % 3 == 0 or i % 5 == 0])
 
+def main():
+	i = int(raw_input("What is the upper bound of the summation of the multiples of 3 or 5?\n  -->  "))
+	print "With an upper bound of %i, the sum is %i" %(i, mults3or5(i))
+main()
 
-print mults3or5(1000)
