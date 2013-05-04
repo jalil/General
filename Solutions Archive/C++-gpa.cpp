22b3154e1c463b164fa735886bd950b55603dacf
Braden <administrator@M4300.(none)>
2013-05-03 22:36:07 -0500

    Added c++ gpa calculator

---
+++ Solutions Archive/C++-gpa.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+double letterGradeToNum(std::string s) {
+	double out;
+	//Use only the first character of the grade -- we will not consider A+, A-, etc.
+	char c = s[0];
+	switch(c) {
+		case 'a':
+		case 'A':
+			out = 4.0;
+			break;
+		case 'b':
+		case 'B':
+			out = 3.0;
+			break;
+		case 'c':
+		case 'C':
+			out = 2.0;
+			break;
+		case 'd':
+		case 'D':
+			out = 1.0;
+			break;
+		default:
+			out = 0.0;
+			break;
+	}
+	return out;
+}
+	
+
+double computeGPA(std::vector<std::pair<std::string, int> > v) {
+	double sumHours, sumGPA;
+	sumGPA = sumHours = 0.0;		
+
+	for (std::vector<std::pair<std::string, int> >::iterator it = v.begin(); it != v.end(); it++) {
+		sumGPA += letterGradeToNum(std::get<0>(*it)) * std::get<1>(*it);
+		sumHours += std::get<1>(*it);
+	}
+	
+	return sumGPA / sumHours;
+}
+
+
+int main(int argc, const char *argv[]) {
+	int i,j,k;
+	std::string grade, in;
+	std::vector<std::pair<std::string, int> > v;
+	
+	std::cout << "How many classes are you taking?\n  -->  ";
+	std::cin >> j;
+
+	for (i = 0; i < j; i++) {
+		std::cout << "\nGive me the letter grade for class " << i + 1 << ": ";
+		std::cin >> grade;
+
+		std::cout << "Give me the number of credit hours: ";
+		std::cin >> k;
+
+		v.push_back(std::make_pair(grade, k));
+	}
+
+	std::cout << "\n\nGiven the classes listed, your GPA is " << computeGPA(v) << std::endl;
+	return 0;
+}
+	
