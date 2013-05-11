64d44109f5e3bec9160a3f879bfcfe426c920251
Braden Lenz <blenz3@uic.edu>
2013-05-11 11:08:31 -0500

    Update to Line counter program

--- Solutions Archive/Python-CountLinesWritten.py
+++ Solutions Archive/Python-CountLinesWritten.py
@@ -1,23 +1,31 @@
 import os
+def summarizeFiles(fExt, path = "./"):
+	root = os.listdir("./")
+	lstlinesums = []
 
-lst = os.listdir(".")
-#print lst
+	for tfile in root:
+		if tfile[len(tfile) - len(fExt):] == fExt:
+			f = open('./' + tfile, 'r')
+			l = f.readlines()
+			lstlinesums.append(len(l))
+			f.close()
 
-lstlinesums = []
+	print "The number of files with the given file extension in the directory is " + str(len(lstlinesums))
+	asum = 0
+	for val in lstlinesums:
+		asum += val
 
-for tfile in lst:
-	if tfile[0] == 'O':
-		f = open('./' + tfile, 'r')
-		l = f.readlines()
-		lstlinesums.append(len(l))
-		f.close()
+	ave = asum / float(len(lstlinesums))
 
-print "The number of files in the directory is " + str(len(lst))
-asum = 0
-for val in lstlinesums:
-	asum += val
+	print "The average number of lines is " + str(ave) 
+	print "The total number of lines is " + str(asum)
+def main():
+	print "\nYour summary of Python files is:"
+	summarizeFiles(".py")
+	
+	print "\nYour summary of C++ files is:"
+	summarizeFiles(".cpp")
+	print "\nYour summary of Ruby files is:"
+	summarizeFiles(".rb")
 
-ave = asum / float(len(lstlinesums))
-
-print "The average number of lines is " + str(ave) 
-print "The total number of lines is " + str(asum)
+main()
