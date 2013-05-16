4723b7981fbe84ea5349b639e71bcdb046f054f3
Braden <administrator@M4300.(none)>
2013-05-16 14:56:18 -0500

    Updated the stair counting function to contain an iterative solution C++

--- Solutions Archive/C++-Stair_Count.cpp
+++ Solutions Archive/C++-Stair_Count.cpp
@@ -12,12 +12,26 @@ int numPermStairs(int n) {
 	}
 }
 
+int altNumPermStairs(int n) {
+	int l1 = 1, l2 = 1, c, i;
+
+	for (i = 0; i < n - 1; ++i) {
+		c = l1 + l2;
+		l2 = l1;
+		l1 = c;
+	}
+	return c;
+}
+
 int main(int argc, const char *argv[]) {
 	int i;
 	cout << "How many stairs are in the staircase?\n  -->  ";
 	cin >> i;
 
 	cout << "With " << i << " stair(s) in the staircase, there are " << numPermStairs(i) << " permutations of one and two steps to reach the top" << endl;
+
+	cout << altNumPermStairs(i) << endl;
+
 	return 0;
 }
 
