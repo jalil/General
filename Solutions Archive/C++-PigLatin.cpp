ddf0f21fb389e1557ae9593ffdfd468fc8b5b6a0
Braden <administrator@M4300.(none)>
2013-05-17 11:38:08 -0500

    Update to Pig latin translator C++

--- Solutions Archive/C++-PigLatin.cpp
+++ Solutions Archive/C++-PigLatin.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <string>
+#define ASCII_DELTA 32
 
+//char toLower;
+
+bool isUpper(char c) {
+	return (c >= 'A' && c <= 'Z');
+}
+
+std::string toLower(std::string s) {
+	int i;
+	for (i = 0; i < s.length(); ++i) {
+		if (isUpper(s[i])) {
+			s[i] += ASCII_DELTA;
+		}
+	}
+	return s;
+}			
 
 bool isVowel(char c) {
 	bool out;
@@ -23,29 +39,51 @@ bool isVowel(char c) {
 std::string toPigLatin(std::string eng) {
 	int i;
 	std::string pLatin = "";
+	eng = toLower(eng);
 
 	for (i = 0; i < eng.length(); ++i) {
 		if (isVowel(eng[i])) {
-			pLatin = eng.substr(i+1, eng.length()) + eng.substr(0, i) + "ay";
+			pLatin = eng.substr(i, eng.length()) + eng.substr(0, i) + "ay";
 			break;
 		}
 	}
 	return pLatin;
 }
 
+std::string translateSentence(std::string sEng) {
+	int i;
+	std::string sPig = "", word;
+	
+	do {
+		int i = sEng.find(" ");
+		if (i != -1) {
+			word = sEng.substr(0, i);
+			sEng = sEng.substr(i + 1, sEng.length());
+		}
+		else {
+			word = sEng;
+			sEng = "";
+		}
+		sPig += " " + toPigLatin(word);
+	} while (sEng != "");
+	
+	return sPig.substr(1, sPig.length());
+}
+
 int main(int argc, const char *argv[]) {
 	std::string e, p;
-	std::cout << "What is the english string you would like translated?\n  -->  ";
-	std::cin >> e;
+	std::cout << "What is the english sentence you would like translated?\n  -->  ";
+	std::getline(std::cin, e);
 	
-	p = toPigLatin(e);
+	p = translateSentence(e);
 	
 	if (p == "") {
-		std::cout << "Sorry, I can't directly translate the word '" << e << "'" << std::endl;
+		std::cout << "Sorry, I can't directly translate the sentence '" << e << "'" << std::endl;
 	}
 	else {
-		std::cout << "The English word '" << e << "' translates to '" << p << "' in pig latin" << std::endl;
+		std::cout << "The English sentence '" << e << "' translates to '" << p << "' in pig latin" << std::endl;
 	}
+	
 	return 0;
 }
 
