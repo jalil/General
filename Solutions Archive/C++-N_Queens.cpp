f21a11a01440ecbf542275fec97156b5d8666aff
Braden <administrator@M4300.(none)>
2013-05-24 10:13:25 -0500

    Finished C++-N_Queens for single solution

--- Solutions Archive/C++-N_Queens.cpp
+++ Solutions Archive/C++-N_Queens.cpp
@@ -7,18 +7,19 @@ class Queens {
 	private:
 		int n;
 		std::vector<int> queenPositions;
-		bool checkDiagonal(int);
-		bool checkRows(int);
+		bool inDirection(int, int, int, int);
+		bool checkDiagonals(int);
 		bool checkCols(int);
 	public:
 		Queens(int);
 		~Queens();
-		void solve(q);
+		bool solve(int);
 		std::string printBoard(void);
 		bool operator==(Queens);
 		std::vector<int> getQueens(void);
 		int getQueen(int);
 		void setQueen(int, int);
+		void removeQueen(int);
 		bool checkPosValidity(int);
 };
 
@@ -32,8 +33,27 @@ Queens::Queens(int numQueens) {
 
 Queens::~Queens() {}
 
-void Queens::solve(int q) {
-	
+bool Queens::solve(int q) {
+	int i;
+	for (i = 0; i < n; ++i) {
+		setQueen(q, i);
+		std::cout << printBoard() << std::endl;
+		if (checkPosValidity(q)) {
+			if (q == n -1 || solve(q + 1)) {
+				return true;
+			}
+		}
+	}
+	removeQueen(q);
+	return false;	
+}
+
+void Queens::removeQueen(int q) {
+	if (q >= 0 && q < n) {
+		queenPositions[q] = -1;
+	}
+}
+
 
 std::string Queens::printBoard(void) {
 	int i,j, q;
@@ -76,14 +96,54 @@ void Queens::setQueen(int i, int v) {
 	}
 }
 
+bool Queens::checkCols(int q) {
+	int currQueenCol = getQueen(q), count = 0;
+	for (std::vector<int>::iterator it = queenPositions.begin(); it != queenPositions.end(); ++it) {
+		if (currQueenCol == *it) {
+			++count;
+		}
+	}
+	return (count == 1);
+}
+
+bool Queens::inDirection(int r, int c, int rD, int cD) {
+	int i;
+	for (i = 1; i < n; ++i) {
+		if ((r + (rD * i) > n) || (r + (rD * i) < 0) || (c + (cD * i) > n) || (c + (cD * i) < 0)) {
+			return false;
+		}
+		else {
+			if (getQueen(r + (rD * i)) == c + (cD * i )) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool Queens::checkDiagonals(int q) {
+	//q == index of queen, queenPositions[q] == queen col
+	if (!(inDirection(q, getQueen(q), 1, 1))) {
+		if (!(inDirection(q, getQueen(q), -1, 1))) {
+			if (!(inDirection(q, getQueen(q), 1, -1))) {
+				if (!(inDirection(q, getQueen(q), -1, -1))) {
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+		
+
 bool Queens::checkPosValidity(int q) {
-	return true;
+	std::cout << "cols = " << checkCols(q) << " and diags = " << checkDiagonals(q) << std::endl;
+	return (checkCols(q) && checkDiagonals(q));
 }
 
 int main(int argc, const char *argv[]) {
 	Queens q(8);
-	q.setQueen(0,1);
-	q.setQueen(1,2);
+	q.solve(0);
 	std::cout << q.printBoard() << std::endl;
 	return 0;
 }
